0643c2bd3bb0f5b77f395d1a3188998abb67686c
amir <amirdhami2025@u.northwestern.edu>
2024-05-28 12:50:05 -0500

    updated from legacy h5

--- train_chatbot.py
+++ train_chatbot.py
@@ -120,5 +120,5 @@ model.compile(optimizer=sgd, loss='categorical_crossentropy',
 #Training and saving the model 
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, 
                  batch_size=5, verbose=1)
-model.save('chatbot_model.h5', hist)
+model.save('chatbot_model.keras', hist)
 
