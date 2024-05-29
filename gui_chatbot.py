c65b53a172ad9ef41363ae72fc81316387994d43
amir <amirdhami2025@u.northwestern.edu>
2024-05-29 11:22:03 -0500

    begin clean sentence

--- gui_chatbot.py
+++ gui_chatbot.py
@@ -18,3 +18,13 @@ warnings.filterwarnings("ignore")
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
+
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern - splitting words into array
+
+    #sentence_words = nltk.word_tokenize(sentence)
+
+    # stemming every word - reducing to base form
+    #sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return #sentence_words
