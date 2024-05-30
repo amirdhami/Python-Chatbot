4f4d7988f72bbb89bd9e58a6ae3cbea3ff553805
amir <amirdhami2025@u.northwestern.edu>
2024-05-30 11:54:23 -0500

    create getResponse

--- gui_chatbot.py
+++ gui_chatbot.py
@@ -65,3 +65,16 @@ def predict_class(sentence):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
 
     return return_list
+
+def getResponse(ints, intents_json):
+
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+
+    for i in list_of_intents:
+
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+
+    return result
